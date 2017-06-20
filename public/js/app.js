6b6fe0d98f0d92353f945c6d24c7dbd4d48be138
Nafis Ahmed <nafisahmedkhan9@gmail.com>
2017-06-20 12:51:09 +0530

    message sending started from server to front end;

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,13 @@ var socket = io();
 
 socket.on('connect', function() {
 	console.log('Connect the socket.io server');
-})
+});
+
+/*socket.emit('message', {
+	text: 'Welcome to the chat application!'
+});*/
+
+socket.on('message', function(message) {
+	console.log('New message: ');
+	console.log(message.text);
+});
