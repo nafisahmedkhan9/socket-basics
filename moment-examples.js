9d218de3d0cde20dcd3f35906109514d5401b4b3
Nafis Ahmed <nafisahmedkhan9@gmail.com>
2017-06-22 12:42:10 +0530

    moment file

---
+++ moment-examples.js
@@ -0,0 +1,15 @@
+var moment = require('moment');
+var now = moment();
+
+// console.log(now.format());
+// console.log(now.format('X'));
+// console.log(now.valueOf());
+
+var timestamp = 1498115352215;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mm a'));
+// now.subtract(1, 'year');
+
+// console.log(now.format());
+// console.log(now.format('MMM do YYYY, h:mma'));
